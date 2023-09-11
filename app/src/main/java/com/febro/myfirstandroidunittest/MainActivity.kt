3fcdc8a160dec005e27ff960795120d362aca8ca
Luis Feitoza <luisfeitoza@ols-br.net>
2023-09-11 12:31:03 -0300

    Initial commit

---
+++ app/src/main/java/com/febro/myfirstandroidunittest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.febro.myfirstandroidunittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.febro.myfirstandroidunittest.R.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
